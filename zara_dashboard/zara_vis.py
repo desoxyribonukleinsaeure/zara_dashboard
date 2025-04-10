aa7880d4582738fa3331135a5bb76a5a5adaa8a6
chi <Laelia.Chi@mail.schwarz>
2025-04-10 10:32:40 +0200

    added comments

--- zara_dashboard/zara_vis.py
+++ zara_dashboard/zara_vis.py
@@ -10,6 +10,9 @@
 # Umsetzung des Dashboards: Laélia
 # Aufwandsteilung für das Dashboard: 90% Laélia, 10% Yaren
 
+
+
+# 0. Notwendige Packages importieren
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -21,18 +24,24 @@ st.title("ZARA Verkaufsdaten")
 st.columns(1)[0].write("")
 st.columns(1)[0].write("")
 
+# 1. Daten laden
 df = pd.read_csv("zara.csv", sep=';')
 
-# Gesamt-Umsatz KPI
+# 2. Gesamt-Umsatz KPI
 df['Revenue'] = df['Sales Volume'] * df['price']
 total_revenue = df['Revenue'].sum()
 
-# Gesamt-Verkäufe KPI
+# 3. Gesamt-Verkäufe KPI
 total_sales_volume = df['Sales Volume'].sum()
 
-# Effektivität von Promotionen KPI
+# 4. Effektivität von Promotionen KPI
 promo = df.groupby(['terms', 'Promotion'])['Sales Volume'].sum().reset_index()
 
+# 4.1 Änderung in Verkaufszahlen pro Produktgruppe berechnen
+# (Die Daten wurden gescrapt, d.h. bilden eine Art Momentaufnahme ab.
+# Ein Produkt kann also in diesen Daten nicht gleichzeitig in Promotion und nicht in Promotion sein.
+# Man kann also die Verkaufszahlen desselben Produktes nicht vor und nach einer Promo vergleichen.
+# Daher wurde auf Produktgruppen-Ebene (Spalte 'terms') verglichen.)
 promotion_impact = {}
 for term in promo['terms'].unique():
     term_data = promo[promo['terms'] == term]
@@ -66,15 +75,19 @@ st.columns(1)[0].write("")
 st.columns(1)[0].write("")
 
 col_season, col_pad, col_section = st.columns([1, 0.2, 1])
-# Verhältnis seasonal/not seasonal
+# 5. Verhältnis seasonal/not seasonal
 with col_season:
     st.subheader('Saisonalität')
     seasonal_counts = df['Seasonal'].value_counts().reset_index()
     seasonal_counts.columns = ['Seasonal', 'Count']
 
     seasonal_mapping = {'Yes': 'Saisonal', 'No': 'Nicht<br>Saisonal'}
+    # Zeilenumbruch in 'Nicht<br>Saisonal', damit die Legende weniger horizontalen Platz einnimmt und
+    # das Ringdiagramm gleichgroß ist wie das für die Sektionen (folgendes Diagramm)
+
     seasonal_counts['Seasonal_Display'] = seasonal_counts['Seasonal'].map(seasonal_mapping)
 
+    # 5.1 Ring-Diagramm zur Darstellung, da es hier um Verhälnisse geht, die insg. 100% ergeben müssen
     fig_seasonal = px.pie(seasonal_counts,
                 names='Seasonal_Display',
                 values='Count',
@@ -85,13 +98,14 @@ with col_season:
 
     st.plotly_chart(fig_seasonal, use_container_width=True)
 
-# Item Häufigkeit pro Section (Gibt es z.B. bei den Dame besonders viele Jacken und bei en Herren besonders viele Schuhe?)
+# 6. Häufigkeit pro Sektion (Fragestellung: Gibt es mehr Artikel in der Damen bzw. Herren Sektion?)
 with col_section:
     st.subheader("Sektionen")
 
     section_counts = df['section'].value_counts().reset_index()
     section_counts.columns = ['Section', 'Count']
 
+    # 6.1 Darstellung
     fig_section_spread = px.pie(section_counts,
                 names='Section',
                 values='Count',
@@ -103,7 +117,8 @@ with col_section:
 
 
 
-# Preis unterschiede der gleichen Produktgruppe zw. beiden Sektionen
+# 7. Preis unterschiede der gleichen Produktgruppe zw. beiden Sektionen
+# (mögl. Fragestellung: Sind Herren-Produktgruppen im Schnitt teuerer/günstiger als Damen-Produktgruppen?)
 st.subheader('Preisvergleich nach Sektion')
 section_price = df.groupby(['section', 'terms'])['price'].mean().reset_index()
 
@@ -111,6 +126,12 @@ df_woman = section_price[section_price['section'] == 'WOMAN'].rename(columns={'p
 df_man = section_price[section_price['section'] == 'MAN'].rename(columns={'price': 'Preis MAN'})
 section_transf = pd.merge(df_woman, df_man, on='terms', how='outer')
 
+# Man wollte zunächst die Preis-Spalte der Damen-Sektion mit der Preis-Spalte der Herren-Sektion substrahieren und diese Differenz plotten.
+# Daher die Auspaltung und das Zusammenführen der Dataframes, da man einfach df[Spalte 1] - df[Spalte 2] machen könnte.
+# Es stellte sich aber heraus, dass dieser Datensatz nur Sweater in der Damen-Sektion hatte.
+# Deswegen hat man sich entschieden, die Durchschnittspreise als gestapeltes Balkendiagramm darzustellen, statt die Differenz.
+
+# 7.1 Darstellung
 fig_section_price = px.bar(section_transf,
              x='terms',
              y=['Preis WOMAN', 'Preis MAN'],
@@ -126,27 +147,39 @@ st.columns(1)[0].write("")
 st.columns(1)[0].write("")
 st.columns(1)[0].write("")
 
-# Ist ein Produkt besonders populär / profitabel
+# 8. Max-Min Produkt-Popularität/-Profitabilität
+# (mögl. Fragestellungen: Ist ein Produkt besonders populär/profitabel?
+# Werden Artikel eher gleichmäßig gekauft oder einige viel, einige fast nicht?)
 st.subheader("Produkt Analyse")
+
+# Whitespace
 st.columns(1)[0].write("")
+
 products = df.groupby('name')[['Sales Volume', 'Revenue']].sum().reset_index()
 
+# 8.1 Meist/wenigste verkaufte Produkt und seine Verkaufszahl
 most_sold_volume = products.loc[products['Sales Volume'].idxmax()]
 most_sold_name = most_sold_volume['name']
 least_sold_volume = products.loc[products['Sales Volume'].idxmin()]
 least_sold_name = least_sold_volume['name']
 
+# 8.2 Produkt, welches am meisten/wenigsten Umsatz gemacht hat und seine Umsatzzahl
 most_profitable = products.loc[products['Revenue'].idxmax()]
 most_profitable_name = most_profitable['name']
 least_profitable = products.loc[products['Revenue'].idxmin()]
 least_profitable_name = least_profitable['name']
 
+# 8.3 Unterschied im Gesamtbeitrag
+# Diese Rechnung dient zum Herausfinden, wie verschieden sie zum gesamten Verkauf/Umsatz beigetragen haben,
+# also ob das beste Produkt viel mehr beigetragen hat als das schlechteste.
+# Maximal-Wert wäre hier 100%, was bedeuten würde, dass das beste Produkt hat den gesamten Verkauf/Umsatz ausgemacht und das schlechteste hat nichts beigetragen.
+# Minimal-Wert wäre hier 0%, was bedeuten würde, dass das beste und schlechteste Produkt gleichviel beigetragen haben.
 sales_volume_range = (products['Sales Volume'].max()/products['Sales Volume'].sum()) - (products['Sales Volume'].min()/products['Sales Volume'].sum())
 revenue_range = (products['Revenue'].max()/products['Revenue'].sum()) - (products['Revenue'].min()/products['Revenue'].sum())
 
 sales_revenue = st.radio("Wähle anzuzeigene Größe aus:", ['Sales Volume', 'Revenue'])
 
-# Layout / Darstellung
+# 8.4 Layout / Darstellung
 col1_pop, col2_pop, col3_pop = st.columns([1, 1, 1])
 if sales_revenue == 'Sales Volume':
     st.metric("Max verkauftes Item:", f"{most_sold_name}")
@@ -169,12 +202,17 @@ if sales_revenue == 'Revenue':
     with col3_pop:
         st.metric("Unterschied im Gesamtbeitrag:", f"{revenue_range:.3f} %")
 
+# Whitespace
 st.columns(1)[0].write("")
 st.columns(1)[0].write("")
 st.columns(1)[0].write("")
 st.columns(1)[0].write("")
 
-# Auswirkung der Product Position auf Anzahl der Verkäufe
+# 9. Auswirkung der Produkt Position auf Anzahl der Verkäufe
+# (Fragestellung: Werden Produkte in der End-Cap bzw. in Front of Store mehr oder weniger gekauft als in der Aisle?)
+# Zu Beachten: End-Cap und Front of Store haben weniger Fläche als Aisle und werden in absoluten Verkaufszahlen wahrscheinlich generell geringer sein.
+# Für die genauste Betrachtung müsste man dasselbe Produkt in derselben Menge an verschieden Positionen platzieren und die Verkaufszahlen beobachten.
+# Da die Daten nur eine Momentaufnahme sind, wurde die Auswirkung der Position hier ebenfalls auf Produktgruppen-Ebene verglichen und dann zusammengefasst.
 st.subheader("Auswirkung der Produkt Positionierung auf Verkaufszahl")
 
 aggregated_df = df.groupby(['terms', 'Product Position'])['Sales Volume'].sum().reset_index()
@@ -184,10 +222,12 @@ base_position = 'Aisle'
 aisle_volume_per_term = aggregated_df[aggregated_df['Product Position'] == base_position].groupby('terms')['Sales Volume'].transform('first').reset_index(drop=True)
 repeated_array = np.repeat(aisle_volume_per_term.values, unique_positions)
 
+# Hinweis: Da Aisle als Basis fungiert, wird dafür immer 0 rauskommen
 aggregated_df['aisle_volume'] = repeated_array
 aggregated_df['change_to_base'] = (aggregated_df['Sales Volume'] / aggregated_df['aisle_volume']) - 1
 position_effect = aggregated_df.groupby(['Product Position'])['change_to_base'].mean().reset_index()
 
+# 9.1 Darstellung
 fig_position = px.bar(position_effect,
              y='Product Position',
              x='change_to_base',
@@ -197,25 +237,29 @@ fig_position = px.bar(position_effect,
 
 st.plotly_chart(fig_position)
 
+# Whitespace
 st.columns(1)[0].write("")
 st.columns(1)[0].write("")
 st.columns(1)[0].write("")
 st.columns(1)[0].write("")
 
-# Welche Produktgruppe am profitablesten / meist verkauft
+# 10. Popularität/Profitabilität nach Produktgruppe
+# (Fragestellung: Welche Produktgruppe ist am profitablesten / wird am meisten verkauft?)
 st.subheader("Meist verkauften / profitablesten Produktgruppen")
+
 product_groups = df.groupby('terms')[['Sales Volume', 'Revenue']].sum().reset_index()
 
-sort_by = st.radio("Sortiere Balken nach:", ['Größe', 'Alphabet'])
 metric_to_plot = st.selectbox("Wähle anzuzeigene Größe aus:", ['Sales Volume', 'Revenue'])
+sort_by = st.radio("Sortiere Balken nach:", ['Größe', 'Alphabet'])
 
 if sort_by == 'Größe':
     product_groups_sorted = product_groups.sort_values(by=metric_to_plot, ascending=False)
 elif sort_by == 'Alphabet':
     product_groups_sorted = product_groups.sort_values(by='terms')
 else:
-    product_groups_sorted = product_groups  # Default sorting
+    product_groups_sorted = product_groups  # Default Reihenfolge
 
+# 10.1 Darstellung
 fig_popular_profit = px.bar(product_groups_sorted,
              x='terms',
              y=metric_to_plot,
